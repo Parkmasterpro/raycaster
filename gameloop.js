03ce38d028ea09d4384b7869338def06ca2cb59a
hiruevan <98787662+hiruevan@users.noreply.github.com>
2022-09-02 19:48:50 -0600

    Update

--- gameloop.js
+++
@@ -1,8 +0,0 @@
-		function forever() {
-			setCanvasInfo();
-			ctx.fillStyle = "black";
-			ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-			game();
-
-			window.requestAnimationFrame(forever);
-		};
